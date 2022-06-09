8795bc6c052c7aac56c76e8bbb3b4d9206d0dc4c
Maximilian Both <35725490+maximilianboth@users.noreply.github.com>
2022-06-09 13:55:55 +0200

    enpis add buildings

--- src/views/ems/indicators/EnPiAHU.vue
+++ src/views/ems/indicators/EnPiAHU.vue
@@ -4,8 +4,16 @@
         <template v-slot:site="{ site }">
             <div v-for="site2 in numberofSites" :key="site2">
                 <div v-if="site === site2">
-                    <SubmodelHeader :submodelName="submodelName" />
-                    <EnPis :submodelId ="submodelId" />
+                    <SubmodelHeader :site ="site2" :submodelName="submodelName" :numberOfBuildings="numberOfBuildings">
+                        <template v-slot:building="{ building }">
+                            <b-card class="shadow p-3 mb-5 bg-white rounded" style="width: 100%;" :title="submodelName">
+                                <hr>
+                                <b-row>
+                                    <EnPis :submodelId ="submodelId" :site="site" :building="building" :buildings="buildings" />
+                                </b-row>
+                            </b-card>
+                        </template>
+                    </SubmodelHeader>
                 </div>
             </div>
         </template>
@@ -41,6 +49,51 @@ export default {
       }
       console.log(numberOfSites)
       return numberOfSites
+    },
+    numberOfBuildings () {
+      const loadSiteInfos = this.$store.getters.loadedSiteInformation
+      // console.log(typeof loadSiteInfos)
+      // console.log(loadSiteInfos)
+      // console.log(this.site)
+      const numberBuildings = []
+      for (const item in loadSiteInfos) {
+        // console.log(loadSiteInfos[item])
+        // console.log(item)
+        numberBuildings.push({
+          key: loadSiteInfos[item][5].key,
+          numberOfSite: loadSiteInfos[item][4].value,
+          numberOfBuildings: loadSiteInfos[item][3].value
+        })
+      }
+      // console.log(numberBuildings)
+      return numberBuildings
+    },
+
+    sites () {
+      // console.log(this.$store.getters.loadedSiteInformation)
+      return this.$store.getters.loadedSiteInformation
+    },
+    buildings () {
+      // console.log(this.sites)
+      // console.log(this.$store.getters.loadedBuildingInformation)
+      const buildings = this.$store.getters.loadedBuildingInformation
+      // console.log(buildings)
+      const buildingsArray = []
+      for (const item in buildings) {
+        buildingsArray.push({
+          numberOfSite: buildings[item][2].value,
+          buildingNumber: buildings[item][3].value,
+          submodelId: buildings[item][4].submodelId
+        })
+      }
+      // console.log(buildingsArray)
+      return buildingsArray
+    },
+    selectSubmodels: function () {
+      for (const i of this.submodels) {
+        console.log(i)
+      }
+      return this.submodels
     }
   },
   created () {
