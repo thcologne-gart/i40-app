f1805f7c9c5b7a5623693ec95fc57b372528e121
Alina Cartus <alina.cartus@outlook.de>
2022-08-04 16:55:41 +0200

    first videos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,36 @@
 <template>
-  <div id = "home">
+  <div class="home">
+    <h1>Homepage</h1>
+
+    <v-btn class="green white--text">
+      <v-icon>mdi-home-analytics</v-icon>
+      <v-span> Energiemanagement</v-span>
+    </v-btn>
+
+    <v-container>
+      <v-layout row wrap justify-space-around align-center>
+        <v-flex xs12 md6>
+          <v-card class="pa-3"> grid system part 1</v-card>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-card class="pa-3">
+            <v-layout row wrap>
+            <v-flex xs10 md8>
+            <div>
+              <v-icon align-center>mdi-home-analytics</v-icon>
+              system part 2</div>
+            </v-flex>
+            <v-flex xs2 md4>
+              <v-btn>See more</v-btn>
+            </v-flex>
+            </v-layout>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </div>
+  <!-- <div id = "home">
     <div>
       <v-card
         id="home"
@@ -15,27 +46,27 @@
         </v-container>
       </v-card>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
 
-export default {
-  data () {
-    return {
-      solutions: [
-        { link: '/emsstart', title: 'Energiemanagement' },
-        { link: '/performance', title: 'Performance Monitoring' },
-        { link: '/buildingperformance', title: 'Building Performance' },
-        { link: '/digitaltwins', title: 'Digital Twins' }
-      ]
-    }
-  }
-}
+// export default {
+//   data () {
+//     return {
+//       solutions: [
+//         { link: '/emsstart', title: 'Energiemanagement' },
+//         { link: '/performance', title: 'Performance Monitoring' },
+//         { link: '/buildingperformance', title: 'Building Performance' },
+//         { link: '/digitaltwins', title: 'Digital Twins' }
+//       ]
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
-#home {
+/* #home {
   margin-top: 20px;
   margin-bottom: 20px;
 }
@@ -44,5 +75,5 @@ export default {
     margin-left: 20px;
     margin-right: 20px;
     width: 40%;
-}
+} */
 </style>
