73189d1a09c0754f48f2cbc17bfa8e49c1116b18
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2022-08-18 17:05:10 +0200

    add management site

---
+++ src/components/ems/management/buildings/ManagementBuilding.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-sheet elevation="2">
+    <v-tabs center-active v-model="tab" background-color="grey lighten-2" slider-color="#FFAC1C" color="#0a322b">
+        <v-tab v-for="submodel in submodels" :key="submodel.id">{{ submodel.name }}</v-tab>
+    </v-tabs>
+    </v-sheet>
+    <v-tabs-items v-model="tab" id="custom-tab-items">
+    <v-tab-item v-for="submodel in submodels" :key="submodel.id">
+        <div v-if="submodel.name == 'Operation'">
+            <OperationBuildings />
+        </div>
+        <div v-else-if="submodel.name == 'Management'">
+            <ManagementBuildings />
+        </div>
+    </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+import OperationBuildings from '@/components/ems/management/buildings/OperationBuildings.vue'
+import ManagementBuildings from '@/components/ems/management/buildings/ManagementBuildings.vue'
+
+export default {
+  data () {
+    return {
+      tab: null
+    }
+  },
+  components: { OperationBuildings, ManagementBuildings },
+  props: {
+    site: Array,
+    submodels: Array
+  }
+}
+</script>
--- src/views/ems/management/ManagementStart.vue
+++ src/views/ems/management/ManagementStart.vue
@@ -1,16 +1,14 @@
 <template>
     <div>
       <EmsChapter :emsChapter="emsChapter" />
-      <EmsGrid :submodels="submodels">
-          <div>Platzhalter
-          </div>
-      </EmsGrid>
+      <GridManagement :sites="sites" :buildings="buildings">
+      </GridManagement>
     </div>
 </template>
 
 <script>
 import EmsChapter from '@/components/ems/EmsChapter.vue'
-import EmsGrid from '@/components/ems/EmsGrid.vue'
+import GridManagement from '@/components/ems/management/GridManagement.vue'
 
 export default {
   name: 'Management',
@@ -19,7 +17,49 @@ export default {
       submodels: []
     }
   },
-  components: { EmsChapter, EmsGrid },
+  computed: {
+    buildings () {
+      // console.log(this.sites)
+      // console.log(this.$store.getters.loadedBuildingInformation)
+      const buildings = this.$store.getters.loadedBuildingInformation
+      console.log(buildings)
+      const buildingsArray = []
+      for (const item in buildings) {
+        buildingsArray.push({
+          numberOfSite: buildings[item][2].value,
+          buildingNumber: buildings[item][3].value,
+          submodelId: buildings[item][4].submodelId,
+          buildingDesignation: buildings[item][1].value
+        })
+      }
+      console.log(buildingsArray)
+      return buildingsArray
+    },
+
+    numberofSites () {
+      const loadInfos = this.$store.getters.loadedOrganizationInformation
+      // console.log(loadInfos)
+      let numberSites
+      for (const item in loadInfos) {
+        if (loadInfos[item].idShort === 'NumberOfSites') {
+          numberSites = loadInfos[item].value
+          // console.log(typeof numberSites)
+        }
+      }
+      const numberOfSites = [0]
+      let i
+      for (i = 1; i < numberSites; i++) {
+        numberOfSites.push(i)
+      }
+      // console.log(numberOfSites)
+      return numberOfSites
+    },
+    sites () {
+      console.log(this.$store.getters.loadedSiteInformation)
+      return this.$store.getters.loadedSiteInformation
+    }
+  },
+  components: { EmsChapter, GridManagement },
   created () {
     this.submodels = [
       { id: 1, name: 'Operation', link: 'management-start/operation' },
