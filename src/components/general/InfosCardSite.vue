ef2d0972fc8a77f411e0d9108aa0769882549966
maximilianboth <maximilian_alexander.both@th-koeln.de>
2022-10-12 12:55:40 +0200

    add inference

--- src/components/general/InfosCardSite.vue
+++ src/components/general/InfosCardSite.vue
@@ -4,6 +4,7 @@
       <div v-if="site[4].value === site2">
         <v-card class ="mx-auto my-8" max-width="90%" elevation="2">
             <v-card-title id="card-title">{{ site[1].value }}</v-card-title>
+            <div>Hiiiiii</div>
         </v-card>
       </div>
     </div>
@@ -11,6 +12,24 @@
 </template>
 
 <script>
+async function query (data) {
+  const response = await fetch(
+    'https://api-inference.huggingface.co/models/mboth/klassifizierungGrundfunktionen',
+    {
+      headers: { Authorization: 'Bearer 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' },
+      method: 'POST',
+      body: JSON.stringify(data)
+    }
+  )
+  const result = await response.json()
+  return result
+}
+
+// query({ 'inputs': 'I like you. I love you' }).then((response) => {
+query({ inputs: 'Heizkreis vorlauftemperatur' }).then((response) => {
+  console.log(JSON.stringify(response))
+})
+
 export default {
   props: {
     site: Array
